a133f9bbb655895ebecc0c658cc7394065126695
VanshiTaSahi <156651924+VanshiTaSahi@users.noreply.github.com>
2025-08-03 14:38:00 +0530

    comitted

---
+++ assignments/personalityquiz/personalityquiz/IntroductionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  personalityquiz
+//
+//  Created by Vanshita Sahi on 30/07/25.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ assignments/personalityquiz/personalityquizTests/personalityquizTests.swift
@@ -0,0 +1,17 @@
+//
+//  personalityquizTests.swift
+//  personalityquizTests
+//
+//  Created by Student on 30/07/25.
+//
+
+import Testing
+@testable import personalityquiz
+
+struct personalityquizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
